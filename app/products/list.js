8b0726668f45d5593ef085903b49b3bb2f678a93
Saurabh Mahajan <saurabh7820@gmail.com>
2017-04-23 01:06:09 +1000

    Product list is now rendered

--- app/products/list.js
+++ app/products/list.js
@@ -1,3 +1,15 @@
 export class List{
-    
+    constructor(){
+        this.products = [];
+        for (let i = 0; i < 20; i++) {
+            let product = {
+                title : `Product ${i + 1}`,
+                description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis faucibus tristique augue a luctus. Sed maximus, diam non congue mollis.',
+                thumbnailUrl:`https://placeimg.com/250/200/tech?image=${i}`,
+                price:Math.floor(Math.random() * 100 + i)
+            };
+
+            this.products.push(product);
+        }
+    }
 }
